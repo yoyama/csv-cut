56f71e4c99a50f051e6dfaa8ba4d904458f15999
yoyama <yoyama.jp@gmail.com>
2016-01-02 14:17:03 +0900

    Add some options.

---
+++ README.md
@@ -0,0 +1,26 @@
+# csv-cut
+
+# csv convert tool
+
+```
+  -i <value> | --input <value>
+     input file name
+  -l <value> | --line <value>
+     line for horizontal split
+  -f <value> | --field <value>
+     output fields. output fields. (ex1) -f 1,2,3 (ex2) -f 2-5 (ex3) -f 3-
+  --no-header-in
+     no header for input
+  --no-header-out
+     no header for output
+  --prefix-out <value>
+     prefix for output file name. default 'out'
+  --suffix-out <value>
+     suffix for output file name. default '.csv'
+  --enc-out <value>
+     output encode (utf | sjis | euc)
+  --crlf-out <value>
+     output crlf  (unix | win | mac)
+  --help
+     prints this usage text
+```
--- src/main/scala/csv-cut.scala
+++ src/main/scala/csv-cut.scala
@@ -5,16 +5,50 @@ import sys.process._
 
 case class Opt(
   inputFile:File,
-  outputPrefix:String = "out", outputSuffix:String = ".csv",
+  outPrefix:String = "out", outSuffix:String = ".csv",
   line:Option[Int] = None, field:Option[List[Int]] = None,
   outFields:Option[String] = None,
   inNoHeader:Boolean = false, outNoHeader:Boolean = false,
   inEnc:Option[String] = None ,outEnc:Option[String] = None,
+  outCRLF:Option[String] = None,
   debugEnable:Boolean = false
 )
+object Opt {
+  val encMap = Map(
+    'sjis -> "Windows-31J",
+    'utf -> "UTF-8",
+    'euc -> "EUC-JP"
+  )
+  val crlfMap = Map(
+    'win -> "\r\n",
+    'mac -> "\r",
+    'unix -> "\n"
+  )
 
+  def getEnc(enc:Option[String]):String = {
+    val v = for{ e <- enc; ee <- encMap.get(Symbol(e))} yield ee
+    v.getOrElse("UTF-8")
+  }
+
+  def getCRLF(crlf:Option[String]):String = {
+    val v = for{ e <- crlf; ee <- crlfMap.get(Symbol(e))} yield ee
+    v.getOrElse("\n")
+  }
+}
+
+
+case class OutFieldInfo(numColumn:Int, fields:List[Int])
+object OutFieldInfo {
+  def field(columns:List[String], outFields:OutFieldInfo):List[String] = {
+    outFields.fields.flatMap{
+      case f if(f <= columns.size && f >= 1) => List(columns(f-1))
+      case _ => Nil
+    }
+  }
+}
 
 object CsvCutMain {
+
   def main(args:Array[String]):Unit = {
     val opt = parseArgs(args)
     val ret = opt.map(run(_)).getOrElse(1)
@@ -26,32 +60,58 @@ object CsvCutMain {
     sys.exit(ret)
   }
 
+  def map2[X,Y,Z](ox:Option[X], oy:Option[Y])(f:(X,Y)=>Z):Option[Z] = {
+    for{
+      x <- ox
+      y <- oy
+    } yield f(x,y)
+  }
+
   def run(opt:Opt):Int = {
-    println(s"run run run ${opt.inputFile.toString}")
+
+    val outFieldInfo = opt.outFields.map(f => getFields(opt.inputFile, f))
+
     val readerAll = CSVReader.open(opt.inputFile).toStream
-    val header = if(opt.inNoHeader) None else  Some(readerAll.head)
-    val reader = if(opt.inNoHeader) readerAll else readerAll.tail 
+    val header_ = if(opt.inNoHeader) None else  Some(readerAll.head)
+    val header = map2(header_, outFieldInfo)(OutFieldInfo.field)
+    val reader_ = if(opt.inNoHeader) readerAll else readerAll.tail
+    val reader = outFieldInfo.map(of => reader_.map(c => OutFieldInfo.field(c, of))).getOrElse(reader_)
+
     val readers:List[Stream[List[String]]] = opt.line match {
       case Some(l) => reader.toStream.grouped(l).toList
       case None => List(reader.toStream)
     }
 
+    val outCsvFormat = new DefaultCSVFormat{
+      override val lineTerminator = Opt.getCRLF(opt.outCRLF)
+    }
     readers.zipWithIndex.map{
       case (r, idx) =>
-        val outName = s"${opt.outputPrefix}%03d${opt.outputSuffix}".format(idx)
-        println(outName)
-        val writer = CSVWriter.open(new FileWriter(new File(outName)))
+        val outName = s"${opt.outPrefix}%03d${opt.outSuffix}".format(idx)
+        Console.err.println(s"$outName")
+        val writer = CSVWriter.open(new File(outName), Opt.getEnc(opt.outEnc))(outCsvFormat)
         if(!opt.outNoHeader) header.map(h => writer.writeRow(h))
         writer.writeAll(r)
-      case others => println(s"ZZ $others.toString")
+      case others => Console.err.println(s"Unkown error:${others.toString}")
     }
     0
+  }
+
+  def countColumn(inputFile:File):Int = {
+    val readerAll = CSVReader.open(inputFile).toStream
+    readerAll.head.size
+  }
+
+  def getFields(inputFile:File, fields:String):OutFieldInfo = {
+    val c = countColumn(inputFile)
+    val o = parseFieldOption(c, fields)
+    OutFieldInfo(c, o)
   }
 
   def parseFieldOption(numCols:Int, fstr:Option[String]):List[Int] = {
     fstr.map(parseFieldOption(numCols, _)).getOrElse( (1 to numCols).toList)
   }
-  
+
   def parseFieldOption(numCols:Int, fstr:String):List[Int] = {
     val list = (1 to numCols).toList
     val flist = fstr.split(",").foldLeft(List[Int]()){(acc,v) => {
@@ -67,12 +127,11 @@ object CsvCutMain {
         case x => Console.err.println(s"Invalid field: $x");  acc
       }
     }}
-    println(flist)
     flist.sorted.distinct.filter(x => if(x >=1 && x <= numCols) true else false)
   }
 
   def parseArgs(args:Array[String]):Option[Opt] = {
-    val parser = new scopt.OptionParser[Opt]("scopt") {
+    val parser = new scopt.OptionParser[Opt]("csv-cut") {
       head("csv-cut", "0.0.1")
       opt[String]('i', "input") action { (x, o) =>
         println(x)
@@ -100,15 +159,42 @@ object CsvCutMain {
           case regex(x)  => success
           case _ => failure(s"Invalid param f $v")
         }
-      } text("output fields")
+      } text("output fields. (ex1) -f 1,2,3 (ex2) -f 2-5 (ex3) -f 3-")
       
-      opt[Unit]("no-header-out") action { (x, o) =>
-        o.copy(outNoHeader = true) 
-      } text("no header for output")
 
       opt[Unit]("no-header-in") action { (x, o) =>
         o.copy(inNoHeader = true) 
       } text("no header for input")
+
+      opt[Unit]("no-header-out") action { (x, o) =>
+        o.copy(outNoHeader = true) 
+      } text("no header for output")
+
+      opt[String]("prefix-out") action { (x, o) =>
+        o.copy(outPrefix = x)
+      } text("prefix for output file name. default 'out'")
+
+      opt[String]("suffix-out") action { (x, o) =>
+        o.copy(outSuffix = x)
+      } text("suffix for output file name. default '.csv'")
+
+      opt[String]("enc-out") action { (x, o) =>
+        o.copy(outEnc = Some(x))
+      } validate { v =>
+        Opt.encMap.get(Symbol(v)) match {
+          case Some(enc)  => success
+          case _ => failure(s"Invalid param enc-out $v")
+        }
+      } text("output encode (utf | sjis | euc)")
+
+      opt[String]("crlf-out") action { (x, o) =>
+        o.copy(outCRLF = Some(x))
+      } validate { v =>
+        Opt.crlfMap.get(Symbol(v)) match {
+          case Some(crlf)  => success
+          case _ => failure(s"Invalid param crlf-out $v")
+        }
+      } text("output crlf  (unix | win | mac)")
 
       opt[Unit]("debug") action { (x, c) =>
         c.copy(debugEnable = true)
